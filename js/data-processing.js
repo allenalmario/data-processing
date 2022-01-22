b1f262214bb5d368ab75ce11eb1f1245c10c5794
Allen Almario <allenalmario@gmail.com>
2022-01-21 18:59:10 -0800

    Cart

---
+++ js/data-processing.js
@@ -0,0 +1,51 @@
+//use location object to access querystring (address bar)
+const queryString = window.location.search;
+
+let myData = '';
+
+let myCart = ''; // will store card details
+let myTotal = 0; // will store total cost
+
+if(queryString != ""){
+    //separate querystring parameters
+    const urlParams = new URLSearchParams(queryString);
+
+    urlParams.forEach(function(value, key) {
+
+        if(key == "Cart"){// process cart
+            switch(value){
+                case "Widget":
+                    myCart += "Widget: $3.99<br />"
+                    myTotal += 3.99;
+                break;
+
+                case "Sprocket":
+                    myCart += "Sprocket: $5.99<br />"
+                    myTotal += 5.99;
+                break;
+
+                case "Thingy":
+                    myCart += "Thingy: $1.99<br />"
+                    myTotal += 1.99;
+                break;
+            }
+        }else{//build shipping info
+            // will replace underscore with spaces
+            key = key.split("_").join(" ");
+
+            myData += `<p>${key}: ${value}</p>`;
+            //console.log(value, key);
+        }
+    });
+
+    myCart = `
+        <p><b>Cart Contents</b></p>
+        <p>${myCart}</p>
+        <p>Total: $${myTotal}</p>
+    `;
+
+    myData = myCart + "<p><b>Shipping Information</b></p>" + myData;
+
+    myData += `<p><a href="index.html">CLEAR</a></p>`
+    document.getElementById("output").innerHTML = myData;
+}
